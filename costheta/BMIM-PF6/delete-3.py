e8b95d4d70dfd85cc627fda93c2ea3c556317ba4
longkunxu <xlkfrank@163.com>
2021-04-24 11:31:20 +1000

    Add files via upload

---
+++ costheta/BMIM-PF6/delete-3.py
@@ -0,0 +1,19 @@
+# to prepeare the csv file for the cos\theta calculations. 
+
+import re
+import os
+import glob
+
+
+##removge extra information
+newallLine = []
+with open("cluster_C8H15N2-deleted-replaced.xyz", "r") as sfile:
+    with open("cluster_C8H15N2-deleted-replaced-cleaned.xyz", "w") as tfile:
+        for line in sfile:
+            if 'O ' in line:
+                newallLine.append(line.strip() + '\n')
+        tfile.writelines(newallLine)
+        tfile.close()
+    sfile.close()
+
+
